1f2217f364cd356edd62ddee4b1b32673dfcd567
emiliorz2 <emiliorz2@icloud.com>
2024-08-25 19:23:59 -0600

    chore: Add Klaviyo script to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Inserta aquí el script de Klaviyo */}
+        <script
+          type="text/javascript"
+          async
+          src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UMSMuy`}
+        ></script>
+      </Head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   );
